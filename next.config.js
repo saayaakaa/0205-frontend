6bc0792f1a84dd08b37874fab42a1588eb323409
saayaakaa <saayaakaa1007@gmail.com>
2025-02-18 01:58:31 +0900

    Fix missing modules issue

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  experimental: {
+    appDir: true,
+  },
   env: {
     NEXT_PUBLIC_API_ENDPOINT: process.env.NEXT_PUBLIC_API_ENDPOINT,
   }
